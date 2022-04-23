2b444fb77aa111620da077ba5adc102924352e42
Sangam Poudel <poudelsangam05@gmail.com>
2022-04-23 22:36:55 +0545

    Customer migration

--- database/migrations/2022_04_22_035327_create_customers_table.php
+++ database/migrations/2022_04_22_035327_create_customers_table.php
@@ -14,11 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id('customer_id');
+            $table->id('id');
             $table->string('name', 60);
-            $table->string('email', 100);
-            $table->enum('gender', ["M", "F", "O"]);
-            $table->text('address');
+            $table->string('address');
+            $table->string('phonenumber');
             $table->date('dob');
             $table->string('password');
             $table->timestamps();
