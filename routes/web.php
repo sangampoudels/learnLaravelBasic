3eebe2661356014a9087180ddabd5ff3f3ab378d
Sangam Poudel <poudelsangam05@gmail.com>
2022-04-24 23:25:12 +0545

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Customer;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DemoController;
 use App\Http\Controllers\PhotoCOntroller;
@@ -58,7 +59,30 @@ Route::post('/component', [ComponentController::class, 'component']);
 
 Route::get('/customer/create', [CustomerController::class, 'create'])->name('customer.create');
 Route::get('/customer/delete/{id}', [CustomerController::class, 'delete'])->name('customer.delete');
+Route::get('/customer/force-delete/{id}', [CustomerController::class, 'forceDelete'])->name('customer.force-delete');
+Route::get('/customer/restore/{id}', [CustomerController::class, 'restore'])->name('customer.restore');
 Route::get('/customer/edit/{id}', [CustomerController::class, 'edit'])->name('customer.edit');
 Route::post('/customer/update/{id}', [CustomerController::class, 'update'])->name('customer.update');
 Route::get('/customer', [CustomerController::class, 'view']);
+Route::get('/customer/trash', [CustomerController::class, 'trash'])->name('customer.trash');
 Route::post('/customer', [CustomerController::class, 'store']);
+
+
+//session
+Route::get('get-all-session', function () {
+    $session = session()->all();
+    p($session);
+});
+
+Route::get('set-session', function (Request $request) {
+    $request->session()->put('name', 'sangam Poudel');
+    $request->session()->put('id', '1');
+    $request->session()->flush('status', 'Success');
+    return redirect('get-all-session');
+});
+
+Route::get('destory-session', function () {
+    session()->forget('name',);
+    session()->forget('id',);
+    return redirect('get-all-session');
+});
