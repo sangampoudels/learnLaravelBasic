84c555cf1f0f5f7ab94a6766915adde57f8a6ea1
Sangam Poudel <poudelsangam05@gmail.com>
2022-04-24 23:24:04 +0545

    customerview

--- resources/views/customer-view.blade.php
+++ resources/views/customer-view.blade.php
@@ -1,7 +1,21 @@
 @extends('layout')
 @section('content')
-{{-- <form action="{{route('customer.create')}}" method="POST"> --}}
     <div class="container">
+        <a href="{{route('customer.create')}}">
+        <button class="btn btn-primary d-inline-block m-2 float-right">Add</button>
+
+
+        <a href="/customer/trash">
+        <button class="btn btn-primary d-inline-block m-2 float-right">go to trash</button>
+       
+       
+        {{-- {{session()->get('name')}} --}}
+        @if(session()->has('name'))
+        {{session()->get('name')}} 
+        @else
+           Guest
+           @endif
+
         <table class="table table border-2">
             <thead>
                 <tr>
@@ -18,11 +32,11 @@
                     <td>{{$customer->name}}</td>
                     <td>{{$customer->address}}</td>
                     <td>{{$customer->phonenumber}}</td>
-                    <td>{{$customer->dob}}</td>
+                    <td>{{$customer->dob,}}</td>
                     <td>
                         <a href="{{route('customer.delete', $customer->id)}}">
                             <button class="btn btn-danger">
-                            Delete
+                            Trash
                         </button>
                         <a href="{{route('customer.edit', $customer->id)}}">
                             <button class="btn btn-danger">
