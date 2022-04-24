10e19b5c89ca7e46590c1a1a2d208b7abf9fead6
Sangam Poudel <poudelsangam05@gmail.com>
2022-04-24 23:21:34 +0545

    Customermodel

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $fillable = [
         'id', 'name', 'address', 'phonenumber', 'dob', 'password',
     ];
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucwords($value);
+    }
+    public function getDobAttribute($value)
+    {
+        return date("d-M-Y", strtotime($value));
+    }
 }
